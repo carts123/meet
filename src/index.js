2e0fa3ae17d7af730cbf84eb5e1027d883d93271
carts123 <72449546+carts123@users.noreply.github.com>
2021-04-21 20:23:52 +0100

    Conversion to PWA

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 import * as atatus from 'atatus-spa';
 
@@ -13,7 +13,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorkerRegistration.register();
+serviceWorker.register();
 
 atatus.config('e4588a58f31b45369a112a8cbd907167').install();
 
